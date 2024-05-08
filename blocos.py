eaaa1b3c345ec22d0c98fe0d73d7796f321efc07
Diego Braga <diebraga0055@gmail.com>
2024-05-08 08:39:10 -0300

    blocos

---
+++ blocos.py
@@ -0,0 +1,9 @@
+# demostracao de blocos
+idade = 25
+maior_de_idade = "Maior de idade"
+menor_de_idade = "Menor de idade"
+
+if idade > 18:
+    print(maior_de_idade)
+else:
+    print(menor_de_idade)
